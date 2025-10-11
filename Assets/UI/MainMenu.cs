629b092ba0effed5db6b9c97a170ddde56f838b9
Bjohnston07 <bwjohnst@uci.edu>
2025-10-11 14:28:10 -0700

    Stylized Main Menu

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class NewMonoBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);

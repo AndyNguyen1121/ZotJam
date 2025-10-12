8961c4f9840bf45dca56ed4a9ba21f4c385975d8
AndyNguyen1121 <andyhoangnguyen2005@gmail.com>
2025-10-12 14:26:48 -0700

    add heaven cutscene

--- Assets/Player/Scripts/PlayerManager.cs
+++ Assets/Player/Scripts/PlayerManager.cs
@@ -113,6 +113,9 @@ public class PlayerManager : MonoBehaviour, IDamageable
     public bool deathSequenceStarted;
     public RenderTexture renderTexture;
 
+    [Header("Heaven Sequence")]
+    public GameObject heavenSequence;
+
     [Header("Audio")]
     public AudioSource overworldTheme;
     public AudioSource underworldTheme;
@@ -163,6 +166,8 @@ public class PlayerManager : MonoBehaviour, IDamageable
             EquipWeapon(testWeapon2);
             TakeDamage(10);
         }
+
+        //enemySpawner.Instance.OnWaveEnd += 
 
     }
 
@@ -286,6 +291,14 @@ public class PlayerManager : MonoBehaviour, IDamageable
     public void ActivateHellSkybox()
     {
         RenderSettings.skybox = underworldSkybox;
+    }
+
+    public void CheckIfTransportToOverworld()
+    {
+        if (currentLocation == PlayerLocation.Hell)
+        {
+            Instantiate(heavenSequence);
+        }
     }
     private void OnDrawGizmos()
     {

2b36adebe36786e421d546a46611053ccc86cc48
Bjohnston07 <bwjohnst@uci.edu>
2025-10-11 17:21:18 -0700

    Wave Counter

--- Assets/UI/WaveCounter.cs
+++ Assets/UI/WaveCounter.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
-
+using TMPro;
 public class WaveCounter : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_Text myTMPText;
     void Start()
     {
         
@@ -11,6 +12,6 @@ public class WaveCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        myTMPText.text = "Wave: " + enemySpawner.Instance.waveCounter;
     }
 }
